ce2757ad4149e3804f2ae1056352c8d8e165403e
Nolan Hughes <nhughes987@gmail.com>
2017-10-31 02:32:10 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,14 @@
 def starts_with_a_vowel?(word)
-
+  # if word.match(/\A[aeiouAEIOU]/) == nil
+  if word.match(/\A[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/) == nil
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan()
 end
 
 def words_five_letters_long(text)
