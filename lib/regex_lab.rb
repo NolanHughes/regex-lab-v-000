2ab77215ba66dae2038249f99872950c996831e1
Nolan Hughes <nhughes987@gmail.com>
2017-10-31 03:05:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,12 +4,6 @@ def starts_with_a_vowel?(word)
   else
     false
   end
-
-  # if word.match(/\A[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/) == nil
-  #   true
-  # else
-  #   false
-  # end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
