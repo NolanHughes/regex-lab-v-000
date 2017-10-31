113fda6c8dd202b25bb98cc7d1a1d19ea59adfd5
Nolan Hughes <nhughes987@gmail.com>
2017-10-31 03:14:18 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,6 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-phone.match(/[0-9]{10}/)
-
+  if phone.match(/[0-9]{10}/)
+    true
+  else
+    false
+  end
 end
